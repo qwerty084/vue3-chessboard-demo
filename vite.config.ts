d458e4bd5c7a8e4207a8a13b13ae3541b7c40ac4
Luca Hendrik Helms <lucahendrik.dev@gmail.com>
2022-10-16 18:55:28 +0200

    added dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue3-chessboard-demo/',
+  base: '/vue3-chessboard-demo',
   build: {
     target: 'esnext',
   },
